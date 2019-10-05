fb90359a8200f5ffba310f31451578200f87c1b9
DelScipio <pmmsoares@gmail.com>
2019-10-05 21:42:16 +0200

    database fix

--- autosync.py
+++ autosync.py
@@ -49,14 +49,14 @@ def subsync(path):
                                         command = "subsync \"%s\" -i \"%s\" > \"%s\" " %(videosource, subsource, subdest)
                                         print(command)
                                         #os.system(command)
-                                        database.write(subtitle)
+                                        database.write(subtitle+ "\n"
                                         reference = subsource
                                         time.sleep(1)
                                     else:
                                         command = "subsync \"%s\" -i \"%s\" > \"%s\" " %(reference, subsource, subdest)
                                         print(command)
                                         #os.system(command)
-                                        database.write(subtitle)
+                                        database.write(subtitle + "\n")
                                         time.sleep(1)
     database.close()
 
